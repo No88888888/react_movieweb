cd191dc55c6bc57f67b3832df910fc3a1cbd96b0
김진호 <wlsghsla94@naver.com>
2023-01-20 00:32:22 +0900

    0120

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,7 @@
-import { useState, useEffect } from "react";
-
+// import { useState, useEffect } from "react";
+// import Movie from "./components/Movie";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`,
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return <div>{loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => )}</div>}</div>;
+  return null;
 }
 
-export default App;
+export default App; // 최초에 렌더링되는 부분
